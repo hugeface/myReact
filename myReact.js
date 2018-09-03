e006498ffe501f8f4b20d2701c4ea48f0ae74f88
Hugeface <756148873@qq.com>
2018-09-03 23:14:12 +0800

    实现视图随数据改变刷新

--- myReact.js
+++ myReact.js
@@ -1,15 +1,26 @@
 var component = {
     root: document.getElementById('root'),
-    data: 'Jack',
+    data: {
+        name: 'Jack'
+    },
     setData: function(key, val) {
         var _t = this;
         _t.data[key] = val;
+        _t.render();
     },
     render: function() {
         var _t = this;
-        var template = `<div>hello, world</div>`;
-        _t.root.innerHTML = template;
+        var template = `<h1>hello, {name}!</h1>`;
+        var html = template.replace(/\{.*\}/g, function (res) {
+            var key = res.substr(1, res.length - 2);
+            return _t.data[key];
+        })
+        _t.root.innerHTML = html;
     }
 };
 
 component.render();
+
+setTimeout(function () {
+    component.setData('name', 'React');
+}, 2000);
